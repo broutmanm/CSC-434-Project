eb7cd8fed1e5617f87f9d746d4460b639cb93ee6
jbartlett <jen10bart@gmail.com>
2022-04-26 13:39:46 -0400

    Delete posts works and clicking on the username works

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Post
+from .models import Post, User
 from . import db
 
 views = Blueprint("views", __name__)
@@ -133,3 +133,26 @@ def create_post():
             flash('The data is submitted', category='success')
             return redirect(url_for('views.view_posts'))
     return render_template('create_post.html', user=current_user)
+@views.route("/delete-post/<id>")
+@login_required
+def delete_post(id):
+    post = Post.query.filter_by(id=id).first()
+    if not post:
+        flash("Post does not exist.", category='error')
+    elif current_user.id == post.id:
+        flash('You do not have permission to delete this post.', category='error')
+    else:
+        db.session.delete(post)
+        db.session.commit()
+        flash('Post deleted', category='success')
+    return redirect(url_for('views.view_posts'))
+
+@views.route("/posts/<username>")
+@login_required
+def posts(username):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        flash('No user with that username exists.', category='error')
+        return redirect(url_for('views.view_posts'))
+    posts=Post.query.filter_by(author=user.id).all()
+    return render_template("posts.html", user=current_user, posts=posts, username=username)
